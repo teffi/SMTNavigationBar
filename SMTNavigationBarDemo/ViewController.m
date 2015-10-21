6952902f16cd545aff9073f46ec7c24478ebfe33
Riza <riza.francisco@jumpdigital.ph>
2015-10-21 11:18:43 +0800

    Added setTitle on navigation bar

--- SMTNavigationBarDemo/ViewController.m
+++ SMTNavigationBarDemo/ViewController.m
@@ -18,7 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     [self willHideBackButton:YES isAlways:YES];
-    self.navigationItem.title = @"FIRST VC";
+   // [self setTitle:@"FIRST"];
     [self createButtons];
 }
 
@@ -63,17 +63,19 @@
 
 -(void)createButtons{
     //CREATE BUTTONS
-    UIButton * leftBtn = [[UIButton alloc]initWithFrame:CGRectMake(0, 0, 44, 44)];
+    UIButton * leftBtn = [[UIButton alloc]initWithFrame:CGRectMake(0, 0, 50, 50)];
     [leftBtn setTitle:@"left" forState:UIControlStateNormal];
     [leftBtn setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
     
-    UIButton * rightBtn = [[UIButton alloc]initWithFrame:CGRectMake(0, 0, 44, 44)];
+    UIButton * rightBtn = [[UIButton alloc]initWithFrame:CGRectMake(0, 0, 50, 50)];
     [rightBtn setTitle:@"right" forState:UIControlStateNormal];
     [rightBtn setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
     
     //Create key value buttons. Access universally via SMTNavigationBar
     [self createButtonWithKey:@"leftBtn" button:leftBtn];
     [self createButtonWithKey:@"rightBtn" button:rightBtn];
+    
+    [self setTitleViewWithImage:[UIImage imageNamed:@"logo.png"]];
     
 }
 
